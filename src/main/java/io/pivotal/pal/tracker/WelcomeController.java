08acef963a36d5e844fd23a6cdaf0a7c8941afe3
pc280 <pc280@ntrs.com>
2020-12-08 15:47:45 +0000

    Assignment Three

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,2 +1,18 @@
-package io.pivotal.pal.tracker;public class WelcomeController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController  {
+    private String message;
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String a_message) {
+        message = a_message;
+    }
+
+    @GetMapping("/")
+    public String sayHello(){
+        return message;
+    }
 }
